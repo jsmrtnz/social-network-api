18848928c5b25e7f021a0b434f1f5a36b793a447
Jose Martinez <jmartinezg.ca@gmail.com>
2020-08-14 22:40:13 -0700

    Add toggle for friend request

--- src/controllers/fr.js
+++ src/controllers/fr.js
@@ -1,59 +1,66 @@
-const FR = require('../models/fr')
+const Fr = require('../models/fr')
 const User = require('../models/user')
 
 exports.sendFriendReq = async (req, res) => {
-    try {
-        const recipient = await User.findById(req.body.recipient)
-        if (!recipient) {
-            throw new Error()
-        }
-        const fr = new FR({
-            from: req.user._id,
-            to: recipient._id
-        })
-        await fr.save()
-        res.send(fr)
-    } catch (e) {
-        res.status(500).send(e)
-    }   
+  try {
+    let request = {}
+    const user = await User.findById(req.query.id)
+    if (!user) {
+      throw new Error()
+    }
+    request = await Fr.findOne({ from: req.user._id, to: user._id })
+    if(request) {
+      request.remove()
+      res.status(202).send()
+    } else {
+      request = new Fr({
+        from: req.user._id,
+        to: user._id
+      })
+      await request.save()
+      res.status(201).send(request)
+    }
+  } catch (e) {
+    res.status(500).send(e)
+  }
 }
 
 exports.acceptDecline = async (req, res) => {
-    try {
-        const request = await FR.findById(req.query.id)
-        if (!request || !req.query.v) {
-            throw new Error()
-        }
-        if (req.query.v === 'accepted') {
-            const sender = await User.findById(request.from)
-            const recipient = await User.findById(request.to)
-            // User sending a request, can not accept the same request.
-            if (sender.equals(req.user)) {
-                return res.status(400).send()
-            }
-            sender.friends.push(recipient)
-            recipient.friends.push(sender)
-            await sender.save()
-            await recipient.save()
-            await request.remove()
-        } else if (req.query.v === 'declined') {
-            await request.remove()
-        } else {
-            return res.status(400).send()
-        }
-        res.send()
-    } catch (e) {
-        res.status(500).send(e)
+  try {
+    const request = await Fr.findById(req.query.id)
+    if (!request || !req.query.v) {
+      throw new Error()
     }
+    if (req.query.v === 'accepted') {
+      const from = await User.findById(request.from)
+      const to = await User.findById(request.to)
+      // User sending a request, can not accept the same request.
+      if (from.equals(req.user)) {
+        return res.status(400).send()
+      }
+      from.friends.push(to)
+      to.friends.push(from)
+      await from.save()
+      await to.save()
+      await request.remove()
+    } else if (req.query.v === 'declined') {
+      await request.remove()
+    } else {
+      return res.status(400).send()
+    }
+    res.send()
+  } catch (e) {
+    res.status(500).send(e)
+  }
 }
 
 exports.frReceived = async (req, res) => {
-    try {
-        await req.user.populate({
-            path: 'friendRequests',
-        }).execPopulate()
-        res.send(req.user.friendRequests)
-    } catch (e) {
-        res.status(500).send()
-    }
+  try {
+    await req.user.populate({
+      path: 'friendRequests',
+    }).execPopulate()
+    res.send(req.user.friendRequests)
+  } catch (e) {
+    res.status(500).send()
+  }
 }
